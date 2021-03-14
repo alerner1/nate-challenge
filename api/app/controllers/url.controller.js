e2df697a0f3cab9515eb5358d9d154785dd3603d
alerner1 <alyssaplerner@gmail.com>
2021-03-14 12:40:31 -0400

    added text processing to backend

--- api/app/controllers/url.controller.js
+++ api/app/controllers/url.controller.js
@@ -1,16 +1,50 @@
 const db = require('../models')
+const rp = require ('request-promise')
+const JSSoup = require("jssoup").default
+const _ = require('lodash')
 const Url = db.urls
 
 exports.createUrl = (req, res) => {
-  console.log(req.body)
-  Url.create({
-    userId: req.userId,
-    urlPath: req.body.urlPath
+  Url.findOne({
+    where: {
+      userId: req.userId,
+      urlPath: req.body.urlPath
+    }
   })
-    .then(() => {
-      res.send({ message: "new url created" })
+    .then(url => {
+      if (!url) {
+        Url.create({
+          userId: req.userId,
+          urlPath: req.body.urlPath
+        })
+          .catch(err => {
+            res.status(500).send({ message: err.message })
+          })
+      }
+      rp(req.body.urlPath)
+        .then(html => {
+          const soup = new JSSoup(html)
+          const text = soup.text
+          result = processText(text)
+          res.status(200).send({ result })
+        })
     })
-    .catch(err => {
-      res.status(500).send({ message: err.message })
-    })
+}
+
+const processText = (text) => {
+  const lowerCaseText = _.lowerCase(text)
+  const words = _.words(lowerCaseText)
+  const frequency = {}
+  for (let word of words) {
+    if (!(word in frequency)) {
+      frequency[word] = 1
+    } else {
+      frequency[word] += 1
+    }
+  }
+
+  return frequency
+
+  // to get the sorted object:
+  // return Object.entries(frequency).sort((a, b) => b[1] - a[1])
 }

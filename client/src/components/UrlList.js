1b83197f0037c423837007cd1f72e5989bec76e8
alerner1 <alyssaplerner@gmail.com>
2021-03-15 12:28:48 -0400

    can view history

--- client/src/components/UrlList.js
+++ client/src/components/UrlList.js
@@ -1,6 +1,11 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
+import UrlPath from './UrlPath';
+import ListGroup from 'react-bootstrap/ListGroup'
+import {v4 as uuidv4} from 'uuid'
 
 const UrlList = () => {
+  const [urlHist, setUrlHist] = useState([])
+
   useEffect(() => {
     const token = localStorage.getItem("token")
     fetch('http://localhost:9000/api/user/urls', {
@@ -9,11 +14,21 @@ const UrlList = () => {
       }
     })
       .then(res => res.json())
-      .then(data => console.log(data))
+      .then(data => {
+        setUrlHist(data.urls)
+      })
   }, [])
 
+  const mapUrls = () => {
+    return urlHist.map(urlObj => {
+      return <UrlPath key={uuidv4()} urlPath={urlObj.urlPath} />
+    })
+  }
+
   return(
-    <p>i had a little linky</p>
+    <ListGroup>
+      {urlHist.length > 0 ? mapUrls() : 'Loading...'}
+    </ListGroup>
   )
 }
 
---
+++ client/src/components/UrlPath.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ListGroup from 'react-bootstrap/ListGroup';
+
+const UrlPath = ({ urlPath }) => {
+  return(
+    <ListGroup.Item>{urlPath}</ListGroup.Item>
+  )
+}
+
+export default UrlPath;
